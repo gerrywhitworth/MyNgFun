c9503c22e89bde1ef8095028e18697b9d400e62a
Gerry Whitworth <gerrywhitworth@sierrasystems.com>
2017-04-12 16:06:16 -0700

    Creating Directives and Advanced Components

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Params } from '@angular/router'
 import { IEvent, ISession, EventService } from '../shared/index'
 
 @Component({
@@ -14,6 +14,7 @@ export class EventDetailsComponent implements OnInit {
   event:IEvent
   addMode: boolean
   filterBy: string = 'all';
+  sortBy: string = 'votes';
 
   constructor(private eventService: EventService, private route: ActivatedRoute) {
 
@@ -36,6 +37,9 @@ export class EventDetailsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
+    this.route.params.forEach((params: Params) => {
+      this.event = this.eventService.getEvent(+params['id']);
+      this.addMode = false;
+    })
   }
 }
